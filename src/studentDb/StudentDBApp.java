6366af5b396b072ed4ad34606fbaf5d0508b2ac7
Dylan Cunningham <gdylanc@gmail.com>
2018-02-25 10:27:40 -0700

    adds basic method calls to demo StudentDB class

--- src/studentDb/StudentDBApp.java
+++ src/studentDb/StudentDBApp.java
@@ -4,7 +4,17 @@ public class StudentDBApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StudentDB stud1 = new StudentDB("Sarah", "1233454324");
+		StudentDB stud1 = new StudentDB("Sarah", "445819456");
+		
+		stud1.enroll("history");
+		stud1.enroll("math");
+		stud1.checkBalance();
+		stud1.pay(1000);
+		stud1.enroll("java 101");
+		stud1.showCourses();
+		stud1.setCity("Tempe");
+		stud1.setState("AZ");
+		stud1.setPhone("4802349876");
 		System.out.println(stud1.toString());
 	}
 
