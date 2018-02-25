15e39e5dc2ed4c4c3abe3c35f81a3a7ab89ae79f
Dylan Cunningham <gdylanc@gmail.com>
2018-02-25 12:17:24 -0700

    demos StudentDB class better

--- src/studentDb/StudentDBApp.java
+++ src/studentDb/StudentDBApp.java
@@ -6,18 +6,22 @@ public class StudentDBApp {
 		// TODO Auto-generated method stub
 		StudentDB stud1 = new StudentDB("Sarah", "445819456");
 		
-		stud1.enroll("history");
-		stud1.enroll("math");
+		stud1.enroll("history302");
+		stud1.enroll("math401");
 		stud1.checkBalance();
-		stud1.pay(1000);
-		stud1.enroll("java 101");
+		stud1.pay(2800);
+		stud1.enroll("java101");
 		stud1.showCourses();
 		stud1.setCity("Tempe");
 		stud1.setState("AZ");
 		stud1.setPhone("4802349876");
 		System.out.println(stud1.toString());
 		
-		StudentDB stud2 = new StudentDB("Michelle", "342097746");
+		StudentDB stud2 = new StudentDB("Michelle", "442349823");
+		stud2.setCity("Scottsdale");
+		stud2.setState("AZ");
+		stud2.setPhone("6023413454");
+		stud2.enroll("Bio202");
 		System.out.println(stud2);
 	}
 
