fd300c6cdc9a11a9526a037e13195ff8bd7bccf6
Dylan Cunningham <gdylanc@gmail.com>
2018-02-25 09:42:19 -0700

    adds main class StudentDBApp

---
+++ src/studentDb/StudentDBApp.java
@@ -0,0 +1,11 @@
+package studentDb;
+
+public class StudentDBApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudentDB stud1 = new StudentDB("Sarah", "1233454324");
+		System.out.println(stud1.toString());
+	}
+
+}
