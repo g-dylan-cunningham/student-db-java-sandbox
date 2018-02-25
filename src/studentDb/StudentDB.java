fd13fae8ef333a07c06479ca9843a3b7c53fd519
Dylan Cunningham <gdylanc@gmail.com>
2018-02-25 09:41:56 -0700

    builds out constructor, toString method, and getRandom method

--- src/studentDb/StudentDB.java
+++ src/studentDb/StudentDB.java
@@ -6,19 +6,38 @@ public class StudentDB {
 	public String userId;
 	private String SSN;
 	public String name;
+	private String email;
 	private String phone;
 	private String city;
 	private String state;
 	private String[] classes;
 	private int balance = 0;
+	private static final String school = "Los Alamos Universityy";
 	
 	// CLASS CONSTRUCTORS
-	public StudentDB() {
+	public StudentDB(String name, String SSN) {
 		// Generate a user ID that is combination of Static ID, random 
 		// 4-digit number between 1000 and 9000, and last 4 of SSN
+		this.SSN = SSN;
+		this.name = name;
+		
+		email = name + "@losAlamos.edu";
+		int randomNum = getRandom();
+		
 	}
 	
 	// CLASS METHODS
+	private int getRandom() {
+		int random = (int) (Math.random() * 10000);
+		if(random > 1000 && random < 9000) {
+			return random;
+		} else {
+			return getRandom();
+		}
+		
+	}
+	
+	
 	public void enroll() {
 		// add class to classes array
 	}
@@ -34,7 +53,7 @@ public class StudentDB {
 	@Override
 	public String toString() {
 		// returns basic student info
-		return "";
+		return "[Name: " + name + "]\n[SSN: " + SSN + "]";
 		
 	}
 	
