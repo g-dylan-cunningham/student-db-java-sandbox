0ff4f9f7938de4ab395986f5995b394505da0e82
Dylan Cunningham <gdylanc@gmail.com>
2018-02-25 10:19:27 -0700

    adds getters/setters for city, state, phone

--- src/studentDb/StudentDB.java
+++ src/studentDb/StudentDB.java
@@ -76,4 +76,28 @@ public class StudentDB {
 			}
 		}
 	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
 }
